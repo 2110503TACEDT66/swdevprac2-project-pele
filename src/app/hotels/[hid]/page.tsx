1869b7755adfabe1661c874f4a89c2832f428d3c
pperatuch <ppezgamer@gmail.com>
2024-03-25 08:53:22 +0700

    pppp

--- src/app/hotels/[hid]/page.tsx
+++ src/app/hotels/[hid]/page.tsx
@@ -9,7 +9,7 @@ export default async function HotelDetailPage( {params} : {params : {hid:string}
         <main className="text-center p-5 ">
             <h1 className="text-lg font-medium text-white">{HotelDetail.data.name} </h1>
             <div className="flex flex-row my-5">
-                <Image src="/img/cover2.jpg" alt="Hospital Image" width={0} height={0} sizes="100vw" className="rounded-lg w-[30%] m-10"/>  
+                <Image src={HotelDetail.data.picture} alt="Hospital Image" width={0} height={0} sizes="100vw" className="rounded-lg w-[30%] m-10"/>  
                 <div className="text-md mx-5 text-left text-white my-10"> {HotelDetail.data.name}
                 <div className=""> Address: {HotelDetail.data.address} </div>
                 <div className=""> Tel: {HotelDetail.data.tel} </div>
