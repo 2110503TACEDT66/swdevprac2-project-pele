2d1534f61c5b5d3825c90210fe73a9908595dce5
pperatuch <ppezgamer@gmail.com>
2024-03-26 02:02:38 +0700

    pepepep

--- src/components/HotelCard.tsx
+++ src/components/HotelCard.tsx
@@ -1,21 +1,17 @@
 'use client'
 import Image from 'next/image'
 
-export default function HotelCard({hotelName , imgSrc} : {hotelName : string , imgSrc : string}) {
+export default function HotelCard({hotelName, hotelAddress, hotelTel } : {hotelName : string, hotelAddress: string, hotelTel : string }) {
 
     const session =''
 
     return (
-        <div className='w-[300px] h-[300px] rounded-lg shadow-lg bg-white'>
-            <div className='w-full h-[70%] relative rounded-t-lg'>
-                <Image src={imgSrc}
-                    alt='Hotel'
-                    fill={true}
-                    className='object-cover rounded-t-lg'/>
-            </div>
-            <div className='w-full h-[25%] p-[10px]'>{hotelName}</div>
+        <div className='border w-[300px] h-[200px] p-4 my-4 mx-2 rounded-xl hover:shadow-lg transition duration-300  ease-in-out transform hover:scale-105 bg-white animate-fade-up animate-once"'>
+            <div className='w-full h[40%]  p-[10px]  text-xl  font-serif '>{hotelName}</div>
+            <div className='w-full h[20%] p-[10px] text-sm'>Address: {hotelAddress}</div>
+            <div className='w-full h[20%] p-[10px] text-sm'>Tel: {hotelTel}</div>
             {
-                session? <button className="w-full h-[5%] block rounded-[30px] px-10 bg-sky-600 hover:bg-indigo-600 py-2 text-white shadow-sm">Booking</button>
+                session? <button className="w-full h[20%] block rounded-[30px] px-10 bg-sky-600 hover:bg-indigo-600 py-2 text-white shadow-sm">Booking</button>
                 : ''
             } 
         </div>
--- src/components/HotelCatalog.tsx
+++ src/components/HotelCatalog.tsx
@@ -7,18 +7,15 @@ export default async function HotelCatalog({hotelsJson} : {hotelsJson:Promise<Ho
     return(
         <>
         choose 1 of {hotelJsonReady?.count} hospitals
-        <div className="m-[20px] justify-around content-around flex flex-row flex-wrap">
+        <div style={{margin:"20px",justifyContent:"space-around", alignContent:"space-around",display:"flex" , flexDirection:"row" , flexWrap:"wrap"}}>
                 {
                     hotelJsonReady?.data.map( (hotelItem:HotelItem)=> (
-                        <Link href={`/hotels/${hotelItem.id}`} className="w-1/5">
-                        <HotelCard hotelName={hotelItem.name} imgSrc={hotelItem.picture}/>
-                        </Link>
+                        <HotelCard hotelName={hotelItem.name} hotelAddress={hotelItem.address} hotelTel={hotelItem.tel} />
                     ))  //Array.map ( (x)=> (ฟังชั่นแสดงไรบ้างล้ะ) ) copy from hospitalpanel
                 }
             </div>  
         </>
     )
 }
-//style={{margin:"20px" ,justifyContent:"space-around", alignContent:"space-around",display:"flex" , flexDirection:"row" , flexWrap:"wrap"}
 
 //ขึ้นแดงๆ เพราะ json เป้น Object ธรรมดา ไม่ได้ทำเป็นคลาส
