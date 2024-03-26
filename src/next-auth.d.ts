c9d17895e7b0e02c69ac7e4a0a18095f37003f9e
puremPP <suprato47@gmail.com>
2024-03-26 14:36:37 +0700

    update next-auth.d.ts

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -3,10 +3,13 @@ import NextAuth from 'next-auth'
 declare module "next-auth" {
     interface Session {
         user: {
-            _id: string,
-            name: string,
-            email: string,
-            role: string,
+            data: {
+                _id: string,
+                name: string,
+                email: string,
+                role: string,
+                token: string
+            }
             token: string
         }
     }
